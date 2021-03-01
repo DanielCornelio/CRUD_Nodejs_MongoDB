047d72eff3c7f1fe02dad2dd04de2e09a78b76ec
Daniel <jdcornelio89@gmail.com>
2021-02-28 21:46:50 -0600

    turning status

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,4 +15,26 @@ router.post('/add', async (req, res)=>{
     await task.save()
     res.redirect('/')
 })
+router.get('/turn/:id', async (req, res)=>{
+    const {id} = req.params
+    const task = await Task.findById(id)
+    task.status = !task.status
+    await task.save()
+    res.redirect('/')
+
+})
+
+router.get('/edit/:id', async (req, res)=>{
+    const {id} = req.params
+    const task = await Task.findById(id)
+    res.render()
+})
+
+
+router.get('/delete/:id', async (req, res)=>{
+        const {id} = req.params
+        await Task.remove({_id:id})
+        res.redirect('/')
+
+})
 module.exports = router
